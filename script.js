e18a46f8797323b3ef8c894f54b47449276338d3
Theeyesign <sejupato@gmail.com>
2024-10-27 12:20:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ function checkCombination() {
     content.innerHTML = "Un mensaje antiguo aparece: 'La primera pista está en el río escondido...'";
   } else if (input === "789-101") {
     content.innerHTML = "Aparece un video en la pantalla...";
-    content.innerHTML += `<video src="video.mp4" controls autoplay></video>`; // Enlace a un video
+    content.innerHTML += `<video src="video.mp4" controls autoplay></video>`;
+  } else if (input === "666-666") {
+    content.innerHTML = "⚠️ Error... mensaje corrompido ⚠️"; // Efecto de error o fallo
   } else {
     content.innerHTML = "Nada sucede... Prueba con otra combinación.";
   }
+
+  content.classList.add("glitch-text");
 }
